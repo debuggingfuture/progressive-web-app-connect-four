82297629af7d064b43b7b0261da8e1f92f4a2903
LCY Vincent <lauchunyin@gmail.com>
2016-05-13 22:10:52 +0800

    fix html require

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ import boardSrvc from './app/board.srvc.js';
 import TwoPlayCtrl from './app/views/2p.ctrl.js';
 import MenuCtrl from './app/views/menu.ctrl.js';
 
-const app = angular.module('app', ['ngRoute', 'ngMaterial'])
+import twoPlayTemplate from './app/views/2p.html';
+import menuTemplate from './app/views/menu.html';
+import aboutTemplate from 'app/views/about.html';
+
+angular.module('app', ['ngRoute', 'ngMaterial'])
 .component('boardCmpt', boardCmpt)
 .service('boardSrvc', boardSrvc)
 .controller('MainCtrl', function() {
@@ -22,80 +26,20 @@ const app = angular.module('app', ['ngRoute', 'ngMaterial'])
 .controller('MenuCtrl', MenuCtrl)
 .config(['$routeProvider',
 function($routeProvider) {
-  $routeProvider.
-  when('/2p', {
-    templateUrl: 'app/views/2p.html',
+  $routeProvider
+  .when('/2p', {
+    templateUrl: twoPlayTemplate,
     controller: 'TwoPlayCtrl',
     controllerAs: 'vm'
-  }).
-  when('/menu', {
-    templateUrl: 'app/views/menu.html',
+  })
+  .when('/menu', {
+    templateUrl: menuTemplate,
     controller: 'MenuCtrl'
-  }).
-  when('/about', {
-    templateUrl: 'app/views/about.html'
-  }).
-  otherwise({
+  })
+  .when('/about', {
+    templateUrl: aboutTemplate
+  })
+  .otherwise({
     redirectTo: '/menu'
   });
 }]);
-
-
-
-
-// app.controller('ListCtrl', function($scope, $mdDialog) {
-//   $scope.toppings = [
-//     { name: 'Pepperoni', wanted: true },
-//     { name: 'Sausage', wanted: false },
-//     { name: 'Black Olives', wanted: true },
-//     { name: 'Green Peppers', wanted: false }
-//   ];
-//   $scope.messages = [
-//     {id: 1, title: "Message A", selected: false},
-//     {id: 2, title: "Message B", selected: true},
-//     {id: 3, title: "Message C", selected: true},
-//   ];
-//   $scope.people = [
-//     { name: 'Janet Perkins', img: 'img/100-0.jpeg', newMessage: true },
-//     { name: 'Mary Johnson', img: 'img/100-1.jpeg', newMessage: false },
-//     { name: 'Peter Carlsson', img: 'img/100-2.jpeg', newMessage: false }
-//   ];
-//   $scope.goToPerson = function(person, event) {
-//     $mdDialog.show(
-//       $mdDialog.alert()
-//       .title('Navigating')
-//       .textContent('Inspect ' + person)
-//       .ariaLabel('Person inspect demo')
-//       .ok('Neat!')
-//       .targetEvent(event)
-//     );
-//   };
-//   $scope.navigateTo = function(to, event) {
-//     //apps state
-//     // switch(to):
-//     //   case '1p':
-//     //   case '2p':
-//     //     console.log('2p');
-//     //   default:
-//     //     return '';
-//     //
-//     // $mdDialog.show(
-//     //   $mdDialog.alert()
-//     //     .title('Navigating')
-//     //     .textContent('Imagine being taken to ' + to)
-//     //     .ariaLabel('Navigation demo')
-//     //     .ok('Neat!')
-//     //     .targetEvent(event)
-//     // );
-//   };
-//   $scope.doSecondaryAction = function(event) {
-//     $mdDialog.show(
-//       $mdDialog.alert()
-//       .title('Secondary Action')
-//       .textContent('Secondary actions can be used for one click actions')
-//       .ariaLabel('Secondary click demo')
-//       .ok('Neat!')
-//       .targetEvent(event)
-//     );
-//   };
-// });
